fb5bf77c61456220528734dc660289868ab967a1
Mohammed Ahmed <mohamedstarwars@gmail.com>
2020-07-05 00:24:28 +0200

    C Files added

---
+++ main.c
@@ -0,0 +1,59 @@
+#include "Macros.h"
+#include "Std_Types.h"
+#include "NVIC_interface.h"
+#include "UART_interface.h"
+#include "GPIO_interface.h"
+#include "SYSCNTRL_interface.h"
+#include "TM4C123GH6PM.h"
+
+void vidReceiveCommands(void);
+
+int main(void)
+{
+	SYSCNTRL_vidEnableGPIOClock(SYSCNTRL_GPIO_PORTB);
+	SYSCNTRL_vidEnableGPIOClock(SYSCNTRL_GPIO_PORTA);
+	SYSCNTRL_vidEnableUARTClock(SYSCNTRL_UART0);
+	
+	GPIO_vidSetPinDirection(GPIO_PORTB,GPIO_PIN0,GPIO_OUTPUT);
+	GPIO_vidSetPinDigEnable(GPIO_PORTB,GPIO_PIN0,GPIO_DEN_SET);
+	
+	GPIO_vidSetPinDirection(GPIO_PORTA,GPIO_PIN0,GPIO_OUTPUT);
+
+	GPIO_vidSetPinDigEnable(GPIO_PORTA,GPIO_PIN0,GPIO_DEN_SET);
+	GPIO_vidSetPinDigEnable(GPIO_PORTA,GPIO_PIN1,GPIO_DEN_SET);
+	
+	UARTConfig_t strctUARTConfig;
+	
+	strctUARTConfig.u8Fraction = 11;
+	strctUARTConfig.u8Integer = 104;
+	strctUARTConfig.u8WordLength = UART_WORDSIZE_8;
+	strctUARTConfig.u8ClockSource = UART_CLOCKSOURCE_RC;
+	
+	/*Initializing UART*/
+	UART_vidInit(&strctUARTConfig);
+	GPIOA->AFSEL |= 0x03;
+	GPIOA->PCTL |= 0x11;
+	UART_vidPutISRFunction(vidReceiveCommands);
+	
+	
+	/*UART receive interrupt*/
+			NVIC->IP[5] = 3 << 5;
+
+	NVIC->ISER[0] |= 0x00000020;
+	__enable_irq();
+
+	while(1);
+}
+
+void vidReceiveCommands(void)
+{
+	u8 u8Byte = UART_u8GetReceivedByte();
+	if (u8Byte == 'h')
+	{
+		GPIO_vidSetPinValue(GPIO_PORTB,GPIO_PIN0,STD_HIGH);
+	}
+	else 
+	{
+		GPIO_vidSetPinValue(GPIO_PORTB,GPIO_PIN0,STD_HIGH);
+	}
+}
