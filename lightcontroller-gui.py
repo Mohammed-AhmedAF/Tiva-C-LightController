b9f9f2fd72d5abc588993ec3d44bd4006f7e5d4d
Mohammed Ahmed <mohamedstarwars@gmail.com>
2021-01-22 19:47:41 +0200

    Putting function definition first

--- lightcontroller-gui.py
+++ lightcontroller-gui.py
@@ -5,42 +5,12 @@ from tkinter import ttk
 import platform
 import serial
 
-#Global variables
-ser = serial.Serial()
-baudrate = 0
-
-root = Tk()
-root.title("Light Controller GUI")
-root.resizable(width=False,height=False)
-
-try:
-    photo = PhotoImage(file="images/led.png")
-    root.iconphoto(False,photo)
-except:
-    print("No photo")
-
-statusLabel = Label(root,text="",relief=SUNKEN,anchor=W)
-osLabel = Label(root,text=platform.system(),relief=SUNKEN,anchor=W)
-
-connectionFrame = LabelFrame(root,text="Connection")
-controlFrame = LabelFrame(root,text="Control")
 
 def sendLED(ledNumber):
     if (ledNumber==0):
         ser.write(b'a')
     elif (ledNumber == 1):
         ser.write(b'b')
-
-led0Label = Label(controlFrame,text="LED 1")
-led0Btn = Button(controlFrame,text="Toggle",command= lambda: sendLED(0))
-
-led1Label = Label(controlFrame,text="LED 2")
-led1Btn = Button(controlFrame,text="Toggle",command= lambda: sendLED(1))
-
-baudrateCmbox = ttk.Combobox(connectionFrame,values=[9600,19200,38400,57600,115200],state="readonly")
-baudrateCmbox.current(0)
-portCmbox = ttk.Combobox(connectionFrame,values=["COM3","COM4"],state="readonly")
-portCmbox.current(1)
 
 def connectByUART():
     global ser
@@ -62,6 +32,37 @@ def connectByUART():
             connectByUART()
         else:
             statusLabel['text'] = "Connection already opened!"
+
+#Global variables
+ser = serial.Serial()
+baudrate = 0
+
+root = Tk()
+root.title("Light Controller GUI")
+root.resizable(width=False,height=False)
+
+try:
+    photo = PhotoImage(file="images/led.png")
+    root.iconphoto(False,photo)
+except:
+    print("No photo")
+
+statusLabel = Label(root,text="",relief=SUNKEN,anchor=W)
+osLabel = Label(root,text=platform.system(),relief=SUNKEN,anchor=W)
+
+connectionFrame = LabelFrame(root,text="Connection")
+controlFrame = LabelFrame(root,text="Control")
+
+led0Label = Label(controlFrame,text="LED 1")
+led0Btn = Button(controlFrame,text="Toggle",command= lambda: sendLED(0))
+
+led1Label = Label(controlFrame,text="LED 2")
+led1Btn = Button(controlFrame,text="Toggle",command= lambda: sendLED(1))
+
+baudrateCmbox = ttk.Combobox(connectionFrame,values=[9600,19200,38400,57600,115200],state="readonly")
+baudrateCmbox.current(0)
+portCmbox = ttk.Combobox(connectionFrame,values=["COM3","COM4"],state="readonly")
+portCmbox.current(1)
 
 connectBtn = Button(connectionFrame,text="Connect",command=connectByUART)
 
